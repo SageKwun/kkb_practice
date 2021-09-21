03b25c72edf2b97ef31fa8f7eeb4e2adaff587bd
SageKwun <sagekwun@gmail.com>
2021-09-21 15:00:42 +0800

    feat(rollup): use plugin in output & add terser

--- day_02/rollup/rollup.config.js
+++ day_02/rollup/rollup.config.js
@@ -1,10 +1,19 @@
 import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "./src/main.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "esm",
-  },
+  output: [
+    {
+      file: "dist/bundle.js",
+      format: "esm",
+    },
+    {
+      file: "dist/bundle.min.js",
+      format: "esm",
+      name: "version",
+      plugins: [terser()],
+    },
+  ],
   plugins: [json()],
 };
